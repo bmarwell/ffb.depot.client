0ce2a7e0425f7ba22e5a4896aca678e7135572be
Benjamin Marwell <bmarwell@gmail.com>
2016-08-27 23:12:08 +0200

    [#6]: LoginResponse => Immutable.

--- src/main/java/de/bmarwell/ffb/depot/client/json/LoginResponse.java
+++ src/main/java/de/bmarwell/ffb/depot/client/json/LoginResponse.java
@@ -22,78 +22,36 @@ package de.bmarwell.ffb.depot.client.json;
 
 import com.google.gson.annotations.SerializedName;
 
-public class LoginResponse {
+import org.immutables.gson.Gson;
+import org.immutables.value.Value;
 
-  private boolean loggedIn;
-  private String username;
-  private String firstname;
-  private String lastname;
-  private String usertype;
+@Value.Immutable
+@Gson.TypeAdapters
+public abstract class LoginResponse {
 
-  @SerializedName("ZustimmungNutzungsbedingungenFFS")
-  private String agbAgreed;
-  private String errormessage;
+  @SerializedName("loggedIn")
+  protected abstract String isLoggedInAsString();
 
   public boolean isLoggedIn() {
-    return loggedIn;
+    return Boolean.parseBoolean(isLoggedInAsString());
   }
 
-  public void setLoggedIn(boolean loggedIn) {
-    this.loggedIn = loggedIn;
+  @SerializedName("username")
+  public abstract String getUsername();
+
+  public abstract String getFirstname();
+
+  public abstract String getLastname();
+
+  public abstract String getUsertype();
+
+  @SerializedName("ZustimmungNutzungsbedingungenFFS")
+  protected abstract String getAgbAgreedAsString();
+
+  public boolean getAgbAgreed() {
+    return Boolean.parseBoolean(getAgbAgreedAsString());
   }
 
-  public String getUsername() {
-    return username;
-  }
-
-  public void setUsername(String username) {
-    this.username = username;
-  }
-
-  public String getFirstname() {
-    return firstname;
-  }
-
-  public void setFirstname(String firstname) {
-    this.firstname = firstname;
-  }
-
-  public String getLastname() {
-    return lastname;
-  }
-
-  public void setLastname(String lastname) {
-    this.lastname = lastname;
-  }
-
-  public String getUsertype() {
-    return usertype;
-  }
-
-  public void setUsertype(String usertype) {
-    this.usertype = usertype;
-  }
-
-  public String getAgbAgreed() {
-    return agbAgreed;
-  }
-
-  public void setAgbAgreed(String agbAgreed) {
-    this.agbAgreed = agbAgreed;
-  }
-
-  public String getErrormessage() {
-    return errormessage;
-  }
-
-  public void setErrormessage(String errormessage) {
-    this.errormessage = errormessage;
-  }
-
-  @Override
-  public String toString() {
-    return "LoginResponse [loggedIn=" + loggedIn + ", username=" + username + ", firstname=" + firstname + ", lastname="
-        + lastname + ", usertype=" + usertype + ", agbAgreed=" + agbAgreed + ", errormessage=" + errormessage + "]";
-  }
+  public abstract String getErrormessage();
 
 }
