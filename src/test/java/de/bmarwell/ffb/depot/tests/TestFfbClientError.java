d776ab0ee465748b27adfa83ef632d996696dd6f
Benjamin Marwell <bmarwell@gmail.com>
2016-08-28 18:06:23 +0200

    Tests for FfbClientError.

---
+++ src/test/java/de/bmarwell/ffb/depot/tests/TestFfbClientError.java
@@ -0,0 +1,50 @@
+package de.bmarwell.ffb.depot.tests;
+
+import de.bmarwell.ffb.depot.client.err.FfbClientError;
+
+import org.junit.Assert;
+import org.junit.Before;
+import org.junit.Test;
+
+public class TestFfbClientError {
+
+  @Before
+  public void setUp() throws Exception {
+  }
+
+  @Test
+  public void testClientError() {
+    try {
+      throw new FfbClientError();
+    } catch (FfbClientError e) {
+      Assert.assertNotNull(e);
+    }
+  }
+
+  @Test
+  public void testClientErrorMessage() {
+    try {
+      throw new FfbClientError("message");
+    } catch (FfbClientError e) {
+      Assert.assertNotNull("Thrown exception should not be null", e);
+      Assert.assertNotNull("Message should not be null.", e.getMessage());
+      Assert.assertNull("Cause was not set.", e.getCause());
+    }
+  }
+
+  @Test
+  public void testClientErrorThrowable() {
+    try {
+      throw new FfbClientError(new RuntimeException());
+    } catch (FfbClientError e) {
+      Assert.assertNotNull("Thrown exception should not be null", e);
+      /* the constructor should set the message to the cause's class name. */
+      Assert.assertNotNull("Message should not be null.", e.getMessage());
+      Assert.assertEquals("Message should be cause's class name.", RuntimeException.class.getName(), e.getMessage());
+
+      Assert.assertNotNull("Cause was set, so it's not null.", e.getCause());
+      Assert.assertEquals("Should have the cause.", RuntimeException.class, e.getCause().getClass());
+    }
+  }
+
+}
